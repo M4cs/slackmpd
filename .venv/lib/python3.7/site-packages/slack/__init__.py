ada9d51b394f51e22fe2035598c5080f1a8aa0df
Max <max@Maxs-MacBook-Pro.local>
2020-03-24 12:07:07 -0400

    Remove vscode and venv

--- .venv/lib/python3.7/site-packages/slack/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-from logging import NullHandler
-
-from slack.web.client import WebClient  # noqa
-from slack.rtm.client import RTMClient  # noqa
-
-# Set default logging handler to avoid "No handler found" warnings.
-logging.getLogger(__name__).addHandler(NullHandler())
